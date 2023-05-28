f7099a50ed845e080be1041f9ab017969c862b9f
Luiz Casagrande <casagrandehluiz@gmail.com>
2023-05-28 20:59:58 -0300

    feature/kubernetes_editor

--- frontend/src/app/system/kubernetes/components/action/kubernetes.components.actions.component.ts
+++ frontend/src/app/system/kubernetes/components/action/kubernetes.components.actions.component.ts
@@ -2,7 +2,6 @@ import {Component, Input} from "@angular/core";
 import {KubernetesSupportService} from "../../support/kubernetes.support.service";
 import {KubernetesSupportNamespaceObjectFilter} from "../../support/kubernetes.support.namespace.object.filter";
 import {KubernetesEditorComponent} from "../../editor/kubernetes-editor.component";
-import {ActivatedRoute} from "@angular/router";
 import {MatDialog} from "@angular/material/dialog";
 
 @Component({
@@ -19,13 +18,11 @@ export class KubernetesComponentsActionsComponent {
 
   clusterId = sessionStorage.getItem("cluster");
 
-  constructor(protected activatedRoute: ActivatedRoute,
-              protected dialog: MatDialog) {
+  constructor(protected dialog: MatDialog) {
   }
 
   edit(): void {
-    const clusterId = this.activatedRoute.snapshot.queryParams['cluster']!;
-    const filter = new KubernetesSupportNamespaceObjectFilter(clusterId, this.object.namespace, this.object.name);
+    const filter = new KubernetesSupportNamespaceObjectFilter(Number(this.clusterId), this.object.namespace, this.object.name);
 
     this.dialog.open(KubernetesEditorComponent, {
       data: {
