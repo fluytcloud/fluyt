55055be985a24a21a805a4b7c82c19fd643e0062
Luiz Casagrande <casagrandehluiz@gmail.com>
2023-05-31 20:14:51 -0300

    kubernetes-editor.component.ts adjustment

--- frontend/src/app/system/kubernetes/editor/kubernetes-editor.component.ts
+++ frontend/src/app/system/kubernetes/editor/kubernetes-editor.component.ts
@@ -35,9 +35,16 @@ export class KubernetesEditorComponent implements OnInit {
 
   apply(): void {
     this.data.service.edit(this.data.filter, this.codemirrorValue)
-      .subscribe(() => {
-        this.messageService.add({severity: 'success', summary: 'Success', detail: 'Changes were successfully applied'});
-        this.dialogRef.close();
+      .subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Changes were successfully applied'
+          });
+          this.dialogRef.close();
+        },
+        error: err => this.messageService.add({severity: 'error', summary: 'Error', detail: err?.error.message})
       });
   }
 }
